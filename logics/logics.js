aefbe6aad87600e9e4877408187618047f0f0915
Abhishek-K-Vishvakarma <gitrepo101@gmail.com>
2025-06-02 22:10:22 +0530

    updated to logics.js file

--- logics/logics.js
+++ logics/logics.js
@@ -61,7 +61,7 @@ const Getuser = async(req, res)=>{
 
 const GetallUsers = async(req, res)=>{
   try {
-    const users = await User.find();
+    const users = await User.find({});
     if (!users || users.length === 0) return res.status(404).json({ message: "No users found!" });
     res.status(200).json({ message: "Users fetched successfully!", data: users, status: "success" });
   } catch (error) {
